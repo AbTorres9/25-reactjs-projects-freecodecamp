35972edf96ab6544b881b5234bd1db118aa94ce1
Abhishek Barthwal <abhi.barthwal9@gmail.com>
2024-05-12 11:50:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Accordian from "./components/accordian";
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      {/* Accordian component */}
+      <Accordian />
+    </>
+  );
+}
+
+export default App;
