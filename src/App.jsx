be7deece13d6d1edba02c0d1e2af14a0b99bd408
Abhishek Barthwal <abhi.barthwal9@gmail.com>
2024-05-13 16:56:29 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from "react";
 import "./App.css";
 import Accordian from "./components/accordian";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
+RandomColor;
 
 function App() {
   return (
     <>
       <h1>Hello</h1>
       {/* Accordian component */}
-      <Accordian />
+      {/* <Accordian /> */}
+      {/* Random color component */}
+      {/* <RandomColor /> */}
+      {/* Star rating component */}
+      {/* <StarRating /> */}
+      {/* Image slider component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </>
   );
 }
